fb341dc720ca9744ffcee985058361829769d690
Seto Elkahfi <setoelkahfi@gmail.com>
2016-03-07 14:24:38 +0700

    Connect table cell ui to code

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -6,4 +6,30 @@
 //  Copyright © 2016 Streetdirectory Pte Ltd. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Meal {
+    
+    // MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+    
+        // Initialize stored properties
+        
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        // Initialize should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    
+    }
+}
--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -11,26 +11,24 @@ import XCTest
 
 class FoodTrackerTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    // MARK: FoodTracker tests
+    // Test to confirm that the Meal initializer returns when no name or negative rating is provided
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testMealInitialization() {
+        
+        // Success case
+        let potentialItem = Meal(name: "Newest meal", photo: nil, rating: 5)
+        XCTAssertNotNil(potentialItem);
+        
+        // Failure case
+        
+        let noName = Meal(name: "", photo: nil, rating: 4)
+        XCTAssertNil(noName, "Empty name is invalid")
+        
+        let badRating = Meal(name: "Really bad rating", photo: nil, rating: -1)
+        XCTAssertNil(badRating, "Negative rating is not allowed")
+        
+        
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+
 }
